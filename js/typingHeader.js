dfa5ea7fcba7f9918091839b5f2b76941c03c78f
Kevin Albarran <recodedkevdev@gmail.com>
2023-12-30 20:05:56 -0800

    finished logic for the typing header

--- js/typingHeader.js
+++ js/typingHeader.js
@@ -1,40 +1,44 @@
 const titles = ["Full-Stack Developer", "React Developer", "Problem Solver", "Life-Long Learner"]
 
-const landingHeader = document.querySelector('#home h1');
+const landingHeader = document.querySelector('#heading');
+
 
 function startTyping(words, htmlEl, timeDelay) {
+    const cursorSpan = document.createElement('span');
+    cursorSpan.id = 'cursor';
+    cursorSpan.textContent = "|";
+
     const copyWords = [...words];
     let currentWord = '';
     let wordIndex = 0;
-    let letterIndex = 0;
+    let letterIndex = 1;
     let wordChangeIntervalId;
 
     function typeLetter() {
-        if (letterIndex === currentWord.length) {
+        if (letterIndex === currentWord.length + 1) {
             clearInterval(wordChangeIntervalId);
             setTimeout(() => {
                 wordIndex++;
+                
                 if (wordIndex === copyWords.length) {
                     return;
                 }
+
                 currentWord = copyWords[wordIndex];
-                letterIndex = 0;
+
+                letterIndex = 1;
                 htmlEl.textContent = '';
                 wordChangeIntervalId = setInterval(typeLetter, timeDelay);
             }, 2000); 
             return;
         }
-
-        // Add the next letter to the html element's text content
-        htmlEl.textContent += currentWord[letterIndex];
-        // Move to the next letter
+        htmlEl.textContent = currentWord.slice(0, letterIndex);
+        htmlEl.appendChild(cursorSpan);
         letterIndex++;
     }
 
-    // Initialize the first word
     currentWord = copyWords[wordIndex];
 
-    // Start the interval to type letters
     wordChangeIntervalId = setInterval(typeLetter, timeDelay);
 }
 
